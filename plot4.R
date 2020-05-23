ae42c2f3c4dc118e591e3911ba83f9b423dd2e1f
Marcello Mazzotti <marcellomazzotti@MacBook-Pro-di-Marcello.local>
2020-05-23 03:43:13 +0200

    plot corrected

--- plot4.R
+++ plot4.R
@@ -12,7 +12,7 @@ unlink(zip)
 #load data
 data <- read.delim("household_power_consumption.txt",
                    header = TRUE,
-                   sep = ";")
+                   sep = ";",na.strings = "?",stringsAsFactors = FALSE)
 
 #remove unused objects
 rm(zip)
@@ -42,19 +42,19 @@ png(filename="plot4.png", width = 480, height = 480)
 par(mfrow = c(2,2))
 
 #1
-with(f_data, plot(as.POSIXct(mydatetime), Global_active_power/500, type="l", xlab = "", ylab = "Global active power (kilowatts)"))
+with(f_data, plot(as.POSIXct(mydatetime), Global_active_power, type="l", xlab = "", ylab = "Global active power (kilowatts)"))
 
 #2
-with(f_data, plot(as.POSIXct(mydatetime), Voltage/90+222, type="l",xlab="datetime",ylab="Voltage",ylim=c(230,246), yaxp=c(234,250,4)))
+with(f_data, plot(as.POSIXct(mydatetime), Voltage, type="l",xlab="datetime",ylab="Voltage"))
 
 #3
-with(f_data, plot(as.POSIXct(mydatetime), Sub_metering_1-2, type="l", xlab = "", ylab = "Energy sub metering", yaxp=c(0,30,3), ylim=c(-1,35)))
-with(f_data, lines(as.POSIXct(mydatetime), Sub_metering_2/4-0.5, type="l", col= "red", xlab = ""))
+with(f_data, plot(as.POSIXct(mydatetime), Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering"))
+with(f_data, lines(as.POSIXct(mydatetime), Sub_metering_2 , type="l", col= "red", xlab = ""))
 with(f_data, lines(as.POSIXct(mydatetime), Sub_metering_3, type="l", col= "blue", xlab = ""))
 legend("topright", col=c("black","red","blue"), legend= c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1)
 
 #4
-with(f_data, plot(as.POSIXct(mydatetime), Global_reactive_power/500, type="l",xlab="datetime",ylab="Global_reactive_power", ylim=c(0.0,0.5)))
+with(f_data, plot(as.POSIXct(mydatetime), Global_reactive_power, type="l",xlab="datetime",ylab="Global_reactive_power"))
 
 dev.off()
 
