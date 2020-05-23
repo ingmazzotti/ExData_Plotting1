3509e66b8277a74f6bdd2a35bb2c52f5a0c729bb
Marcello Mazzotti <marcellomazzotti@MacBook-Pro-di-Marcello.local>
2020-05-23 03:37:25 +0200

    correction in reading file

--- plot3.R
+++ plot3.R
@@ -12,7 +12,7 @@ unlink(zip)
 #load data
 data <- read.delim("household_power_consumption.txt",
                    header = TRUE,
-                   sep = ";")
+                   sep = ";",na.strings = "?",stringsAsFactors = FALSE)
 
 #remove unused objects
 rm(zip)
@@ -39,8 +39,9 @@ f_data <- mutate(f_data, mydatetime = as.POSIXct(strptime(paste(f_data$Date, f_d
 
 #create png plot
 png(filename="plot3.png", width = 480, height = 480)
-with(f_data, plot(as.POSIXct(mydatetime), Sub_metering_1-2, type="l", xlab = "", ylab = "Energy sub metering", yaxp=c(0,30,3), ylim=c(-1,35)))
-with(f_data, lines(as.POSIXct(mydatetime), Sub_metering_2/4-0.5, type="l", col= "red", xlab = ""))
+
+with(f_data, plot(as.POSIXct(mydatetime), Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering"))
+with(f_data, lines(as.POSIXct(mydatetime), Sub_metering_2 , type="l", col= "red", xlab = ""))
 with(f_data, lines(as.POSIXct(mydatetime), Sub_metering_3, type="l", col= "blue", xlab = ""))
 legend("topright", col=c("black","red","blue"), legend= c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1)
 dev.off()
